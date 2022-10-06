4d82f3d87004d4c3e376242d1f35d361e2d98c40
Matthew Cook <matthewcook13987@gmail.com>
2022-10-06 13:39:58 -0500

    adds portrait image to project and styles/animtes homepage text

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import './index.scss';
+import Portrait from '../../assets/portrait.jpeg';
 
 const Home = () => {
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hello!<br /> I'm Matthew!<br />Full-Stack Web Developer!</h1>
+                <h1>Hello!<br /> I'm Matthew!<img src={Portrait}></img><br />Full-Stack Web Developer!</h1>
                 <h2>Front-end, Backend etc...</h2>
                 <Link to='/contact' className="flat-button">Contact Me!</Link>
             </div>
