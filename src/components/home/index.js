616e757a33b8b3790689cdcb9d3df7465dc1f80f
Matthew Cook <matthewcook13987@gmail.com>
2022-10-06 13:08:09 -0500

    adds basic intro text with animation styling

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,3 +1,17 @@
-const Home = () => ('hello from home')
+import { Link } from "react-router-dom";
+import './index.scss';
+
+const Home = () => {
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hello!<br /> I'm Matthew!<br />Full-Stack Web Developer!</h1>
+                <h2>Front-end, Backend etc...</h2>
+                <Link to='/contact' className="flat-button">Contact Me!</Link>
+            </div>
+        </div>
+    );
+}
 
 export default Home
