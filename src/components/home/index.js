64699e0741b1b42b61134e429d994f202e3c31f6
Matthew Cook <matthewcook13987@gmail.com>
2022-10-07 13:28:44 -0500

    refactors CSS styling and cleans up animations

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,40 +1,52 @@
 import { Link } from "react-router-dom";
 import './index.scss';
 import Portrait from '../../assets/portrait.jpeg';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AnimatedLetters from "../animatedLetters";
 
 const Home = () => {
 
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //       setLetterClass('text-animate-hover')
+    //     }, 4000)
+    //   }, [])
+
     const [letterClass, setLetterClass] = useState('text-animate');
-    const nameArray = ['M','a','t','t','h','e','w',];
+    
+    const nameArray = [' ','M','a','t','t','h','e','w',];
     const jobArray = ['W','e','b',' ','D','e','v','e','l','o','p','e','r']
 
     
 
-    return (
+    return (<>
+        
         <div className="container home-page">
             <div className="text-zone">
-                <h1>
-                    <span className={letterClass}> H</span>
-                    <span  className={`${letterClass} _12`}>e</span>
-                    <span  className={`${letterClass} _13`}>l</span>
-                    <span  className={`${letterClass} _14`}>l</span>
-                    <span  className={`${letterClass} _16`}>o</span>
-                    
-                <br /> I'm <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15}
-                /><img src={Portrait}></img><br />Full-Stack <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                idx={15}
-                /></h1>
-                
+            <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            /><img src={Portrait}/>
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1> 
                 <h2>Front-end, Backend etc...</h2>
                 <Link to='/contact' className="flat-button">Contact Me!</Link>
+                <Link to='/projects' className="flat-button">Portfolio</Link>
             </div>
         </div>
-    );
+        </>);
 }
 
 export default Home
