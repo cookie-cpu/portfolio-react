c68ea79bc775d2ea00797928178adceb7c362876
Matthew Cook <matthewcook13987@gmail.com>
2022-10-07 14:33:08 -0500

    css cleanup for newly created contact page

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -5,7 +5,10 @@ const Contact = () => {
     return (
         <>
             <div className='container contact-page'>
-                
+            <div className='text-zone'>
+                <h1>Contact me!</h1>
+                <p>Full-Stack Web Development Bootcamp Graduate looking for work</p>
+                </div>
             </div>
             <Loader type="pacman"></Loader>
         </>
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,10 +1,14 @@
 import './index.scss'
 import Loader from 'react-loaders'
+import AnimatedLetters from '../animatedLetters'
 
 const Projects = () => {
     return (
         <>
             <div className='container projects-page'>
+                <div className='text-zone'>
+                <h1>projects</h1>
+                </div>
                 
             </div>
             <Loader type="pacman"></Loader>
