f99ad1d10ed0626ebacdd1e75011e4b318e59cfb
Matthew Cook <matthewcook13987@gmail.com>
2022-10-18 14:51:13 -0500

    adds map to dynamically render project data into card components

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -2,16 +2,68 @@ import './index.scss'
 import Loader from 'react-loaders'
 import ProjectCard from '../projectcard'
 import img1 from '../../assets/projects/portfoliosite.png'
+// import ProjectData from '../../assets/projectData.json';
 
 
-const Projects = () => {
+export default function Projects () {
+    let data = [
+        { id: 1, title: "Portfolio Site", 
+        description: "a website to display my projects (site you are currently on)", 
+        stack: ['react', 'scss', 'material UI', 'heroku'], 
+        img: img1, 
+        demo: "https://matthewcook-portfolio.herokuapp.com/", 
+        repo:"https://github.com/cookie-cpu/portfolio-react"},
+
+        { id: 2, title: "Other Site", 
+        description: "descript", 
+        stack: ['html', 'css', 'javscript'], 
+        image: '../../assets/projects/portfoliosite.png', 
+        demo: "https://matthewcook-portfolio.herokuapp.com/", 
+        repo:"https://github.com/cookie-cpu/portfolio-react"},
+
+        // { id: 2, title: "Johnny", description: "male", stack: ['html','css'], image: "", demo: "", repo:""},
+        // { id: 3, title: "Johnny", description: "male", stack: ['html','css'], image: "", demo: "", repo:""},
+      ];
+
+
+
+    // console.log(ProjectData);
+
+    // const projectList = ProjectData.map((project) => {
+    //     return (
+    //         <ProjectCard 
+    //         title={project.title}
+    //         description={"a website to display my projects (site you are currently on)"}
+    //         stack={['react', 'scss', 'material UI', 'heroku']}
+    //         img={img1}
+    //         demo={"https://matthewcook-portfolio.herokuapp.com/"}
+    //         repo={"https://github.com/cookie-cpu/portfolio-react"}
+    //     />
+    //     )
+    //   })
+    
+
+    const projectList = data.map((project) => {
+        return (
+            <ProjectCard 
+                key={project.id}
+                title={project.title}
+                description={project.description}
+                stack={project.stack}
+                img={project.img}
+                demo={project.demo}
+                repo={project.repo}
+            />
+        )
+      })
+
     return (
         <>
             <div className='container projects-page'>
                 <h1 className='title'>Projects</h1>
-
+                {/* <p style={{fontSize: 24, textAlign: "center", paddingTop: "75px",}}>test</p> */}
                     <div className='projects'>
-                        
+                        {projectList}
                         
                         { /* CARD TEMPLATE
                         <BasicCard 
@@ -23,7 +75,7 @@ const Projects = () => {
                             repo={"link2"}
                         /> */}
 
-                        <ProjectCard 
+                        {/* <ProjectCard 
                             title={"Portfolio Site"}
                             description={"a website to display my projects (site you are currently on)"}
                             stack={['react', 'scss', 'material UI', 'heroku']}
@@ -39,17 +91,11 @@ const Projects = () => {
                             // img={img1}
                             // demo={"/"}
                             // repo={"/"}
-                        />
-
+                        /> */}
                     </div>
-                    
-
-
-
             </div>
+
             <Loader type="pacman"></Loader>
         </>
     )
 }
-
-export default Projects
