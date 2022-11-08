5e83b66364c735cc39324a2261375e733a30f97d
Matthew Cook <matthewcook13987@gmail.com>
2022-11-08 12:05:01 -0600

    animation changes for resume page

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -6,7 +6,7 @@ export default function Resume() {
   return (
     <>
         <div className='resume-div'>
-            <iframe src="https://resume.creddle.io/embed/7unjh2qlpud" width="850" height="750" seamless></iframe>
+            <iframe className='resume' src="https://resume.creddle.io/embed/7unjh2qlpud" width="850" height="775" seamless></iframe>
         </div>
         <Loader type='pacman'/>
     </>
