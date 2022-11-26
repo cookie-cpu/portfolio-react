0018f3200a68bad713e86251313d2ff2cb5c38aa
Matthew Cook <matthewcook13987@gmail.com>
2022-11-26 14:13:30 -0600

    fixes animated letters component

--- src/components/animatedLetters/index.js
+++ src/components/animatedLetters/index.js
@@ -1,15 +1,15 @@
-import './index.scss'
+import './index.scss';
 
-const AnimatedLetters = ({ letterClass, strArray, idx }) => {
+const AnimatedLetters = ({ letterClass, strArray, index }) => {
   return (
     <span>
       {strArray.map((char, i) => (
-        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+        <span key={char + i} className={`${letterClass} -${i + index}`}>
           {char}
         </span>
       ))}
     </span>
-  )
-}
+  );
+};
 
-export default AnimatedLetters
+export default AnimatedLetters;
